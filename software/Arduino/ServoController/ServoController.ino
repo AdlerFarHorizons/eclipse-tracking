35c17b673f35b5b095948ce4c2c56533267409cd
Max <mabowman200@gmail.com>
2017-03-18 12:31:57 -0500

    Reverted back to old arduino software.

--- software/Arduino/ServoController/ServoController.ino
+++ software/Arduino/ServoController/ServoController.ino
@@ -1,7 +1,7 @@
 /**********************************************************************
  * Arduino Uno Program - Controls Servo and Reads Sensors             *
  * Project: Eclipse Tracking (2017)                                   *
- * Version: 3/04/2017                                                 *
+ * Version: 1.0 (2/18/2017)                                           *
  * Max Bowman / Jeremy Seeman                                         *
  **********************************************************************/
 #include <Servo.h>
@@ -13,40 +13,46 @@
 // Declare global variables
 LSM9DS1 imu; // imu chip
 Servo xy; // x-y plane servo
+int j = 0; // for maintaining the rolling average array
+float oldAverage; // to compute angular movement
 int servoVal = 90; // initial value to set servo at
-float oldHeading = 90;
-float newHeading;
 bool int_flag = false; // interrupt flag for executing gyro read every 100 ms
-const int LED = 13; // indicator LED - subject to change (depends on board)
-int samplingRate = 30;
-int motorFlag = 0;
-
+const int LED = 13; // indicator LED
 void setup() {
-  Serial.begin(11520);
+  Serial.begin(115200);
+
   pinMode(LED, OUTPUT);
   setupIMU(); // set up hardware
+
+  for (int i = 0; i < 10; i++) {
+    entries[i] = readGyro();
+    runningSum += entries[i];
+  } // fill up the rolling average
+
+  oldAverage = getAverage();
   
   xy.attach(3); // pro mini / uno pwm pin
   xy.write(servoVal); // Start at a 90 degrees
 
   blinkLED(50, 10); // Let the use know everything initialized
 
-  MsTimer2::set(samplingRate, a);
-  MsTimer2::start(); // set up and start a timer interrupt to run every 100 ms
+  MsTimer2::set(100, a);
+  MsTimer2::start(); // set up and start a timer interrupt
   
 }
 
 void loop() {
   if (int_flag) {
-    newHeading = getHeading();
-    servoVal += newHeading - oldHeading;
-    oldHeading = newHeading;
-    int_flag = false;
-    motorFlag++;
-  }
-  motorFlag %= 4;
-  if (motorFlag == 0) {
+    j %= 10;
+    runningSum -= entries[j];
+    entries[j] = readGyro();
+    runningSum += entries[j];
+    float newAverage = getAverage();
+    servoVal += newAverage - oldAverage;
     xy.write(servoVal);
+    oldAverage = newAverage;
+    j++;
+    int_flag = false;
   }
 }
 
@@ -55,47 +61,31 @@ void a() {
 }
 
 void setupIMU() {
+  Serial.print("Setting up IMU...");
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
   while (!imu.begin()) {
-      // could not connect - pulse the LED every 250 ms until the imu connects
-      blinkLED(250, 1);
-  }
+    blinkLED(500, 1);
+  }  
 }
 
-float getHeading() {
-  float result = 0;
-  if (imu.magAvailable()) {
-    imu.readMag();
+float readGyro() {
+  if (imu.gyroAvailable()) {
+    imu.readGyro();
   }
-  float magY = imu.my;
-  float magX = imu.mx;
-  if (magY == 0) {
-    // if magnetometer y vector is 0, then the heading is either 180 degrees or 0 degrees
-    if (magX < 0) result = PI;
-    // otherwise, start at 0.
-  }
-  else {
-    result = atan2(magX, magY); // illustration for why is on elgg
-  }
-  result -= DECLINATION * PI / 180.0; // convert declination to degrees and subtract it from the result
-
-  // adjust values to appropriate range
-  if (result > PI) result -= 2 * PI;
-  else if (result < -PI) result += 2 * PI;
-  else if (result < 0) result += 2 * PI;
-
-  result *= 180.0 / PI; // convert result to degrees
-  return result;
+  return imu.calcGyro(imu.gz);
 }
 
 void blinkLED(int del, int n) {
   for (int i = 0; i < n; i++) {
-    digitalWrite(LED, HIGH);
+    digitalWrite(13, HIGH);
     delay(del);
-    digitalWrite(LED, LOW);
+    digitalWrite(13, LOW);
     delay(del);
   }
 }
 
+float getAverage() {
+  return runningSum / 10;
+}
