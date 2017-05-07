6fac08f04e8ac629c7a78140616df45c0a868b48
mabadler <mabowman200@gmail.com>
2017-05-07 08:55:16 -0500

    Update ui.py

--- software/Pi/ui.py
+++ software/Pi/ui.py
@@ -1,13 +1,13 @@
 """
 Handles LED output for the Raspberry Pi 3
 Image tracking software. Imported using 'import ui'
-Version: 5/01/17
+Version: 5/06/17
 Dependencies: RPi.GPIO
 Note: will only work on a Raspberry Pi!
 """
 import RPi.GPIO as gpio
 
-ledPin = 7
+ledPin = 16 #GPIO23
 
 #Set up RPi GPIO
 def setup():
