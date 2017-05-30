93e42e2521fcb2abb26f1eb6ae21182b60012a74
Max Bowman <mabowman200@gmail.com>
2017-05-30 16:14:36 -0500

    Finds sun initially.

--- software/Pi/process.py
+++ software/Pi/process.py
@@ -2,10 +2,10 @@
 Detects brightest region in image and finds angles needed to rotate
 to center it based on GoPro 4 FOV
 Algorithms: Gaussian Blur and Min/Max Loc
-Python Version: 2.7
+Python Version: 2.7.12
 See trello for a comprehensive description of the communication protocol
 Dependencies: PySerial, Numpy, time, gopro_wifi_api_hero4, log, ui, and OpenCV
-Version: 5/13/17
+Version: 5/29/17
 """
 
 import numpy as np
@@ -23,24 +23,27 @@ heightAngle  = 7.66
 widthPixels  = 1280
 heightPixels = 720
 
-sunThreshold = 0 # to be determined by experimentation
+sunThreshold = 230 # to be determined by experimentation
 
 conversion   = widthAngle / widthPixels
 center       = (widthPixels / 2, heightPixels / 2)
 
 myPort = None
 
-#Returns true if the sun is in the image
-def sun_in_image():
+#Returns a black and white image with a Gaussian Blur
+def get_image():
     api.capture()
     api.transfer_latest_photo()
     subprocess.Popen('mv *.JPG image.JPG', shell=True)
     image = cv2.imread("image.JPG")
     subprocess.Popen('rm *.JPG', shell=True)
-
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (41, 41), 0)
+    return gray
 
+#Returns true if the sun is in the image
+def sun_in_image():
+    gray = get_image()
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
     if maxVal >= sunThreshold:
         return True
@@ -61,30 +64,26 @@ def setup():
 def main():
     while True:
         # Retrieve image
-        api.capture()
-        api.transfer_latest_photo()
-        subprocess.Popen('mv *.JPG image.JPG', shell=True)
-        image = cv2.imread("image.JPG")
-        subprocess.Popen('rm *.JPG', shell=True)
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (41, 41), 0)
+        gray = get_image()
 
         (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
 
         result = ""
 
         delta = int((maxLoc[0] - center[0]) * conversion)
+        str_delta = str(delta)
+
         if (delta == 0):
             result = '0000'
         elif delta < 0:
             result += '1'
-            delta = abs(d)
+            delta = abs(delta)
         else:
             result += '0'
-        for i in range(0, 3 - len(str(delta))):
+
+        for i in range(0, 3 - len(str_delta)):
             result += '0'
-        result += str(delta)
+        result += str_delta
 
         try:
             myPort.write(bytes(result))
